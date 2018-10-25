1e90afa8f91fef187aed27077da77daa6585a584
Dilum Sanjaya <dilumsanjaya@gmail.com>
2018-10-25 20:59:36 +0530

    Changed the directories

--- project/app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from app.models import Character, CoOccur, Book
-
-admin.site.register(Character)
-admin.site.register(CoOccur)
-admin.site.register(Book)
--- project/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path,include
-from . import views
-urlpatterns = [
-    path('', views.home, name='app-home'),
-    path('about/', views.about, name='app-about'),
-    path('characters/', views.all_characters, name='app-characters'),
-    path('occurrences/', views.all_occurrences, name='app-occurrences'),
-]
